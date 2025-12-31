792c38c417865490284e0d7105f208e4f1c9c7f2
Simon Daykin <99761925+rocket5imon@users.noreply.github.com>
2025-12-31 21:10:01 +0000

    fix: enable version manifest generation

--- .release.config.js
+++ .release.config.js
@@ -11,16 +11,15 @@ module.exports = {
       }
     ],
 
-    // Write src/_data/version.json and confirm it's visible
+    // Write src/_data/version.json on each release
     [
-      "semantic-release-exec",
+      "@semantic-release/exec",
       {
         prepareCmd:
           "mkdir -p src/_data && echo '{\"version\": \"${nextRelease.version}\", \"released\": \"${nextRelease.gitTag}\", \"notes\": \"${nextRelease.notes}\"}' > src/_data/version.json && cat src/_data/version.json"
       }
     ],
 
-    // Commit updated files
     [
       "@semantic-release/git",
       {
@@ -34,7 +33,6 @@ module.exports = {
       }
     ],
 
-    // Upload version.json as a GitHub release asset
     [
       "@semantic-release/github",
       {
